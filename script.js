ab0705b8db7bd39a631cec9442266e08523ea139
dwikyfebryan <dwikybalies43@gmail.com>
2023-09-01 16:14:45 +0700

    upload ketiga

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Selamat Datang di Website Saya");
+
+document.querySelector(".text-red").innerHTML
+document.querySelector(".text-green").innerHTML
+document.querySelector(".text-blue").innerHTML
+function changeColor()  {
+    document.querySelector(".text-red").style.color = "red";
+    document.querySelector(".text-green").style.color = "green";
+    document.querySelector(".text-blue").style.color = "blue"; }
